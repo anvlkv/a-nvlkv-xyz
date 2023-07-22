e23b7e8c20404de1f91528b4168ba4df4e8a3402
a.nvlkv <a.nvlkv@protonmail.ch>
2023-07-22 22:23:22 +0700

    portfolio pages

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
-from accelerate.utils import write_basic_config
+# from accelerate.utils import write_basic_config
 
-write_basic_config()
+# write_basic_config()
 
+audio_dataset = Dataset.from_dict({"audio": ["path/to/audio_1", ..., "path/to/audio_n"]}).cast_column("audio", Audio())
