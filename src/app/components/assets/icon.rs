0fc5d80dab5d5d2227a099356e84bc6150b39e89
a.nvlkv <2813794+anvlkv@users.noreply.github.com>
2024-05-20 23:19:52 +0300

    update: icons

--- src/app/components/assets/icon.rs
+++ src/app/components/assets/icon.rs
@@ -25,7 +25,7 @@ pub fn IconView(#[prop(into)] icon: MaybeSignal<String>) -> impl IntoView {
         <i
             class=move || {
                 let icon = Icon::from_str(icon.get().as_str()).expect("icon name");
-                format!("uiw-{icon} mx-1")
+                format!("uiw-{icon} mx-0.5")
             }
         ></i>
     }
